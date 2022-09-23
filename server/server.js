d89c499b874fb4523f9c17f3235c7afbe5d1de04
prathamesh thakare <prathamesh06pt@gmail.com>
2022-09-23 13:20:07 +0530

    changed server.js

--- server/server.js
+++ server/server.js
@@ -27,7 +27,7 @@ console.log("Something");
 if(process.env.NODE_ENV="production"){
   app.use(express.static(path.join(__dirname,"../client/build")));
  
-    app.get("*",(req,res)=>res.send(path.resolve(__dirname,"client","build","index.html")))
+    app.get("*",(req,res)=>res.sendFile(path.resolve(__dirname,'../','client',"build","index.html")))
   
 }
 app.listen(PORT,()=>{
